1d9597eb84b31c6729ad6746bc76c3030262ff98
Mabu Rahman Habibullah <habibulm.gmatc.matc.edu>
2026-02-16 19:33:21 -0600

    Completed Activity 3

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
+  require(`dotenv`).config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 /**
  * @type {import('gatsby').GatsbyConfig}
@@ -23,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: '1k1foearbusq',
-        accessToken: 'BMZcHNRQu_2KezEfimqRu5u7d6sRFPTtquBZ8Q7uwBc'
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN
       },
     },
     `gatsby-plugin-image`,
@@ -35,6 +38,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
